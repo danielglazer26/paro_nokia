2afd5a44058c596f7506bbeaf058cd0d9cb5ea1b
danielglazer26 <danielglazer200026@gmail.com>
2022-03-29 21:21:58 +0200

    Add example3.cpp solution

--- memory_managment/example3.cpp
+++ memory_managment/example3.cpp
@@ -1,45 +1,44 @@
 #include <iostream>
 #include <stdexcept>
 
-void validateArguments(int argc)
-{
-    if(argc != 2)
-    {
+class CustomLogicError : public std::logic_error {
+public:
+    explicit CustomLogicError(const std::string &arg) : logic_error(arg) {
+    }
+};
+
+class Resource {
+public:
+    void use(const char *arg) {
+        std::cout << "Using resource. Passed " << *arg << std::endl;
+        if (*arg == 'd') {
+            throw CustomLogicError("Passed d. d is prohibited.");
+        }
+    }
+};
+
+void validateArguments(int argc) {
+    if (argc != 2) {
         std::cerr << "You need to pass 1 argument" << std::endl;
         exit(-1);
     }
 }
 
-class Resource
-{
-public:
-    void use(const char* arg)
-    {
-        std::cout << "Using resource. Passed " << *arg << std::endl;
-        if (*arg == 'd')
-        {
-            throw std::logic_error("Passed d. d is prohibited.");
-        }
-    }
-};
-
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     validateArguments(argc);
 
-    const char* argument = argv[1];
-    Resource* rsc = nullptr;
+    const char *argument = argv[1];
+    Resource *rsc = nullptr;
 
-    try
-    {
+    try {
         rsc = new Resource();
         rsc->use(argument);
-        delete rsc;
     }
-    catch (std::logic_error& e)
-    {
+    catch (CustomLogicError &e) {
         std::cout << e.what() << std::endl;
     }
+    delete rsc;
+
     return 0;
 }
 
